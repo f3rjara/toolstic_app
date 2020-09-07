aa732a588bb21dc55c6c8d2a1a9193e7fe03ac8e
fernando.jaramillo <fernando.jaramillo@branch.com.co>
2020-09-07 16:43:45 -0500

    feature sesione y seguridad de acceso a cuestionario para estudiante

--- main/student/cuestionario.php
+++ main/student/cuestionario.php
@@ -1,16 +1,25 @@
 <?php
-    session_start();
-    if(isset($_SESSION['btnPresentaPrueba']) && $_SESSION['btnPresentaPrueba'] == TRUE ) {
-        echo "<br> hola guapuritas la variable es true y esta habilitado a presentar la prueba ";
+    include_once ($_SERVER['DOCUMENT_ROOT'].'/config_.php');
+    include (ROOT_INCLUDE."/connect.php");  
+    include_once (ROOT_INCLUDE.'/fetch_array.php'); 
+    include_once (ROOT_MAIN.'/views/sesion_student.php'); 
+    // SI EL USUARIO NO ESTA LOGUEADO Y QUIERE ACCEDER
+    if( !isset($_SESSION['error_user']) || ($_SESSION['error_user'] != FALSE && $_SESSION['user_student'] === NULL))   
+    { header('Location: '.ROOT_MEDIA_USER.'/');  }   
+    // COMPROBACION DEL ESTUDIANTE SI INTERACTUA CON LOS BOTONES 
+    if( isset($_SESSION['btnPresentaPrueba'] ) && $_SESSION['btnPresentaPrueba'] == TRUE ) {        
         if(isset($_SESSION['UserInteraction']) && $_SESSION['UserInteraction'] === TRUE) {
+            echo "<br> btnPresentaPrueba variable es TRUE y esta habilitado a presentar la prueba ";
             echo "<br> El usuario SI interactuo";
         }
         else {
-            echo "<br> El usuario pailaaaaa REDIRECCINN ";
+            // EL USUARIO NO REALIZA INTERACION CON EL BOTON DEL CUESTIONARIO
+            $_SESSION['UserInteraction'] = FALSE;
+            header('Location: '.ROOT_MEDIA_USER.'/');
         }
-    }
-    else {
-        echo "<br> La variable es FALSE y  NO esta habilitado  REDIRECCION";
+    } else {
+        // EL USUARIO NO TIENE EL BOTON HABILITADO
+        header('Location: '.ROOT_MEDIA_USER.'/');
     }
 
 ?>
